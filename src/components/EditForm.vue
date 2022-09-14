b9f1526de181e05468242cf0cbde83bd2a2d504c
jack <jackcheah2@gmail.com>
2022-09-14 14:30:30 +0800

    Create EditForm component

---
+++ src/components/EditForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex row items-center justify-between w-full py-1 px-4 my-1 rounded border bg-gray-100 text-gray-600">
+    <input v-model="edited" class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" />
+    <button @click="saveEdit(edited, todo)" class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Save</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'EditForm',
+  props: {
+      todo: String,
+      saveEdit: Function,
+  },
+  data() {
+    return {
+      edited: this.todo.task
+    }
+  }
+}
+</script>
+  
