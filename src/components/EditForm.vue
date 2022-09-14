ab209b7b7b825d36d62a5e45e3f825b472f2c938
jack <jackcheah2@gmail.com>
2022-09-14 15:33:23 +0800

    Edit placeholder

--- src/components/EditForm.vue
+++ src/components/EditForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex flex-row mb-5">
-    <input v-model="edited" class="basis-3/4 bg-transparent border-0 border-b-2 rounded-none p-3 focus:outline-none border-b-green-500 placeholder:text-gray-400 shadow-2xl" @keyup.enter="saveEdit(edited, todo)" placeholder="Edit your task"/>
+    <input v-model="edited" class="basis-3/4 bg-transparent border-0 border-b-2 rounded-none p-3 focus:outline-none border-b-green-500 placeholder:text-gray-400 shadow-2xl" @keyup.enter="saveEdit(edited, todo)" placeholder="Write your task"/>
     <button @click="saveEdit(edited, todo)" class="ml-4 border-none p-4 px-6 shadow-2xl basis-1/4 bg-green-500 hover:bg-green-600 text-white rounded-r-lg">
       Save
     </button>
