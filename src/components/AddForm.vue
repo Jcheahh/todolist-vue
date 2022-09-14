5db6d83912bb0641916536da0d26abd34d373854
jack <jackcheah2@gmail.com>
2022-09-14 15:17:04 +0800

    Adding UI for AddForm and EditForm using tailwind

--- src/components/AddForm.vue
+++ src/components/AddForm.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="flex row items-center justify-between w-full py-1 px-4 my-1 rounded border bg-gray-100 text-gray-600">
-    <div class="items-center column">
-      <input type="checkbox" @click="completeTodo(index)"/>
-      <span class="ml-5 w-full text-grey-darkest" :class="(todo.is_done) ? 'line-through text-gray-400 font-bold' : ' '">{{ todo.task }}</span>
-    </div>
-    <div class="items-center row-reverse">
-
-      <button @click="editTodo(index)"  class="fa-solid fa-pen-to-square text-green-500 cursor-pointer">Edit</button>
-      <button @click="removeTodo(index)" class="px-4 py-1 text-sm text-red-600 font-semibold rounded-full border border-red-200 hover:text-white hover:bg-red-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2">Remove</button>
-    </div>
-    
+  <div class="task_animation w-full bg-slate-900 p-3 my-3 border-0 border-l-4 border-red-500 rounded-r-lg flex justify-between items-center shadow-2xl" :class="(todo.is_done) ? 'border-green-500' : ' '">
+      <span @click="completeTodo(index)" class="cursor-pointer text-gray-400 overflow-hidden w-[80%] h-6 relative" :class="(todo.is_done) ? 'line-through text-gray-400 font-bold' : ' '">
+        {{ todo.task }}
+      </span>
+      <button @click="editTodo(index)" class="text-green-500 mr-4 cursor-pointer">
+        Edit
+      </button>
+      <button @click="removeTodo(index)" class="text-red-500 cursor-pointer">
+        Remove
+      </button>
     
   </div>
 </template>
@@ -27,3 +26,22 @@ export default {
   }
 }
 </script>
+
+<style>
+.task_animation {
+  animation: task_animation .3s cubic-bezier(0, 0, 0.2, 1) forwards;
+}
+
+
+@keyframes task_animation {
+  0% {
+      transform: scale(.5);
+      opacity: 0;
+  }
+
+  100% {
+      transform: scale(1);
+      opacity: 1;
+  }
+}
+</style>
--- src/components/EditForm.vue
+++ src/components/EditForm.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="flex row items-center justify-between w-full py-1 px-4 my-1 rounded border bg-gray-100 text-gray-600">
-    <input v-model="edited" class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" />
-    <button @click="saveEdit(edited, todo)" class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Save</button>
+  <div class="w-full flex flex-row mb-5">
+    <input v-model="edited" class="basis-3/4 bg-transparent border-0 border-b-2 rounded-none p-3 focus:outline-none border-b-green-500 placeholder:text-gray-400 shadow-2xl" placeholder="Edit your task" />
+    <button @click="saveEdit(edited, todo)" class="ml-4 border-none p-4 px-6 shadow-2xl basis-1/4 bg-green-500 hover:bg-green-600 text-white rounded-r-lg">
+      Save
+    </button>
   </div>
 </template>
 
